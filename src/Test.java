cdc59c4c3c5be25c2899e9936f86c149ed13cd18
Indiver Kumar <ink@censhare.com>
2017-09-04 03:44:09 +0530

    MASTER

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
  public static void main(String[] args) {
 	System.out.println("MASTER");
 	System.out.println("MASTER2");
+	System.out.println("MASTER-INK");
 }
 	static void print(){
 		System.out.println("FEATURE#01");
